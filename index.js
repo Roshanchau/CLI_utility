ccc7cf6183d813b5767aa451fe4fe4d175494e75
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-07-16 21:25:01 +0545

    commnad line utility

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('cli tool')
+const fs=require("fs");
+
+module.exports.writehtml5Roshan=writeHTML5;
+
+function writeHTML5(){
+    const html5=fs.readFileSync(`${__dirname}/sukuna.html`)
+
+    const filename=process.argv[2]
+    fs.writeFileSync(`${process.cwd()}/${filename}` , html5)
+}
+
+writeHTML5();
